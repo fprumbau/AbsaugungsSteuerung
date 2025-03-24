c0f323ed22343db234f12a5fafd63879bd116661
Frank Prumbaum <fprumbau@gmail.com>
2025-03-24 19:23:13 +0100

    update

--- AbsaugungsSteuerung.ino
+++ AbsaugungsSteuerung.ino
@@ -7,7 +7,7 @@ int scrollOffset = 0;
 unsigned long lastPacketTime = 0;  // Zeit des letzten empfangenen Pakets
 
 void setup() {
-  debugLevel = LORA_MSGS | DEBUG_CONFIG | DEBUG_WIFI;
+  debugLevel = LORA_MSGS | DEBUG_LORA;
 
   Serial.begin(115200);
   while (!Serial) delay(10);
@@ -53,17 +53,49 @@ void setup() {
 }
 
 void loop() {
-  debugPrint(DEBUG_INIT, "Loop Start...");
+    debugPrint(DEBUG_INIT, "Loop Start...");
 
-  if (updater.getUpdating()) {
-    updater.loop();
-    oled.clear();
-    oled.drawString(0, 0, "OTA Update läuft...");
-    oled.display();
-    return;
-  }
+    if (updater.getUpdating()) {
+      updater.loop();
+      oled.clear();
+      oled.drawString(0, 0, "OTA Update läuft...");
+      oled.display();
+      return;
+    }
+
+    static unsigned long lastConfirmTime = 0;
+    static uint8_t confirmCount = 0;
+    static uint8_t lastSensorId = 0;
+    static bool confirming = false;
+
+    uint8_t sensorId, action;
+    if (lora.receive(sensorId, action)) {
+        if (action == 1) { // "starte"
+            debugPrint(LORA_MSGS, "Received from sensor" + String(sensorId) + ": starte, sending started");
+            confirming = true;
+            lastSensorId = sensorId;
+            confirmCount = 0;
+            lastConfirmTime = 0; // Sofort erste Bestätigung senden
+        }
+    }
+
+    if (confirming && (millis() - lastConfirmTime >= 2000)) {
+        if (confirmCount < 3) {
+            if (lora.send(3)) { // "started"
+                debugPrint(LORA_MSGS, "Confirmation sent to sensor" + String(lastSensorId) + ": started (" + String(confirmCount + 1) + "/3)");
+                confirmCount++;
+                lastConfirmTime = millis();
+            } else {
+                debugPrint(LORA_MSGS, "Confirmation send failed to sensor" + String(lastSensorId));
+            }
+        }
+        if (confirmCount >= 3) {
+            confirming = false;
+            confirmCount = 0;
+        }
+    }
 
 
-  debugPrint(DEBUG_INIT,"Loop Ende...");
-  delay(500);
+    debugPrint(DEBUG_INIT,"Loop Ende...");
+    delay(100); // Reduziert, um Empfang zu beschleunigen
 }
